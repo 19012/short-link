1dd80c3fafb7d78a5fa335fc6bce5d85cf033c5c
qdo_ln <zaytsevia@gmail.com>
2025-02-22 21:06:41 +0700

    feat/config config impoemented

--- cmd/short-link/main.go
+++ cmd/short-link/main.go
@@ -1,7 +1,14 @@
 package main
 
+import (
+	"19012/short-link/internal/config"
+	"fmt"
+)
+
 func main() {
-	// TODO: config(cleanenv)
+	// export CONFIG_PATH=~/go/short-link/config/local.yaml
+	cfg := config.MustLoad()
+	fmt.Println(cfg) // TODO: delete this line
 	// TODO: logger(slog)
 	// TODO: storage(sqlite)
 	// TODO: router(chi, chi render)
