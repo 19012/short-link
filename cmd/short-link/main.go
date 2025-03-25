8409994e7376d37a048cdcc35d80e0d346f0e4d8
qdo_ln <zaytsevia@gmail.com>
2025-03-25 15:57:42 +0300

    logger implemented

--- cmd/short-link/main.go
+++ cmd/short-link/main.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"19012/short-link/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-	// TODO: init config (cleanenv)
 	cfg := config.MustLoad()
-	_ = cfg
-	// TODO:init logger (log/slog)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting short-link", slog.String("env", cfg.Env))
 
 	// TODO: init db (sqlite)
 
 	// TODO: init router (chi, chi render)
 
 	// TODO: launch server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
